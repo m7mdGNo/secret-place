f414da47498d014a4f533fde1ed465b124faf22a
m7mdGNo <m7mdelganainy2001@gmail.com>
2021-07-15 10:09:47 +0100

    'fdsf'

--- account/models.py
+++ account/models.py
@@ -7,11 +7,6 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
-    img = models.ImageField(null=True,blank=True)
-
-
-
-
 
 
     def __str__(self):
