719c01f6281735ad203d9e180258265c2c42d2e0
m7mdGNo <m7mdelganainy2001@gmail.com>
2021-07-15 10:38:56 +0100

    'fdsfdfdsfssafd'

--- account/models.py
+++ account/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     username = models.CharField(max_length=20)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
 
 
     def __str__(self):
