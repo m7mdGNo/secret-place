6cab389e1db537bee63ed8f5d52933581baa9968
m7mdGNo <m7mdelganainy2001@gmail.com>
2021-07-15 10:00:43 +0100

    'fgf'

--- sarahah/settings.py
+++ sarahah/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whiltenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sarahah.urls'
