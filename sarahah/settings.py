249c0b3bd204f33859e373c0ad071e9526547e2f
m7mdGNo <m7mdelganainy2001@gmail.com>
2021-07-15 10:23:42 +0100

    'fadsfad'

--- sarahah/settings.py
+++ sarahah/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +135,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
